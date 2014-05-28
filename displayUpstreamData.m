9398b72d69195ad4cf6533551fb834245130d4f1
andrewcyung <ayung@mail.ubc.ca>
2014-05-28 11:50:07 -0700

    implemented view cycling thru upstreamData array
    can now advance upstreamDataset in the upstreamData figure.  Zoom
    implemented with right-click on upstream Figure

--- displayUpstreamData.m
+++ displayUpstreamData.m
@@ -1,22 +1,53 @@
 function displayUpstreamData(upstreamDataArray,row,col,h_fig_upstream,parname)
 
-upstreamData = upstreamDataArray{1};
-set(h_fig_upstream,'Name',[parname ' upstreamData: ' upstreamData.name]);
-upstreamDataFilename = [upstreamData.dirpath upstreamData.srcfile upstreamData.filetype];
+hManager = uigetmodemanager(h_fig_upstream);
+set(hManager.WindowListenerHandles,'Enable','off');
+set(h_fig_upstream,'KeyPressFcn',@GotoAnotherUpstreamDataset);
+
+n_upstream = length(upstreamDataArray);
+i_upstream = 1;
+
+    function GotoAnotherUpstreamDataset(src,event)
+        switch event.Character
+            case {','}
+                if i_upstream == 1
+                    i_upstream = n_upstream;
+                else
+                    i_upstream = i_upstream-1;
+                end
+            case {'.'}
+                if i_upstream == n_upstream
+                    i_upstream = 1;
+                else
+                    i_upstream = i_upstream+1;
+                end
+        end
+        displaySingleUpstreamDataset(upstreamDataArray{i_upstream},row,col,h_fig_upstream,parname)
+    end
+
+displaySingleUpstreamDataset(upstreamDataArray{1},row,col,h_fig_upstream,parname);
+
+end
+
+function displaySingleUpstreamDataset(upstreamDataset,row,col,h_fig_upstream,parname)
+
+set(h_fig_upstream,'Name',[parname ' upstreamData: ' upstreamDataset.name]);
+upstreamDataFilename = [upstreamDataset.dirpath upstreamDataset.srcfile upstreamDataset.filetype];
 if exist(upstreamDataFilename,'file')
-    if strcmp(upstreamData.filetype,'.mat')
-        if isKey(upstreamData.options,'varname')
-            varname = upstreamData.options('varname');
+    if strcmp(upstreamDataset.filetype,'.mat')
+        if isKey(upstreamDataset.options,'varname')
+            varname = upstreamDataset.options('varname');
             try
                 loadedVar = load(upstreamDataFilename,varname);
             end
             dispQuantity = loadedVar.(varname);
         end
-    elseif strcmp(upstreamData.filetype,'.png') || strcmp(upstreamData.filetype,'.tif')
+    elseif strcmp(upstreamDataset.filetype,'.png') || strcmp(upstreamDataset.filetype,'.tif')
         dispQuantity = imread(upstreamDataFilename);
     end
     dispFcn = [];
-    eval(['dispFcn = @' upstreamData.dispFcnName ';']);
-    dispFcn(h_fig_upstream,row,col,upstreamData.im2src_tfm,dispQuantity,upstreamData.options);
+    eval(['dispFcn = @' upstreamDataset.dispFcnName ';']);
+    dispFcn(h_fig_upstream,row,col,upstreamDataset.im2src_tfm,dispQuantity,upstreamDataset.options);
 end
+
 end
